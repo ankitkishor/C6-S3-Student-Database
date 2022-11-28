42b8dacdcd5481110993ad0db48902224bc0bcf2
ankit <ankit.kishorelv@gmail.com>
2022-11-28 16:43:25 +0530

    added instance variable and construct a parameterized constructor

--- src/main/java/com/niit/jdp/model/StudentData.java
+++ src/main/java/com/niit/jdp/model/StudentData.java
@@ -8,4 +8,38 @@
 package com.niit.jdp.model;
 
 public class StudentData {
+    private int  rollNo;
+    private String name;
+    private String grades;
+
+    public StudentData(int rollNo, String name, String grades) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGrades() {
+        return grades;
+    }
+
+    public void setGrades(String grades) {
+        this.grades = grades;
+    }
+
 }
