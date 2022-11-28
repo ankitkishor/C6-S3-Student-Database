f9cd30e62c9acd6982ef88d9581597f1803aeb21
ankit <ankit.kishorelv@gmail.com>
2022-11-28 17:13:26 +0530

    establish connection to the database

--- src/main/java/com/niit/jdp/repository/StudentSalesRepository.java
+++ src/main/java/com/niit/jdp/repository/StudentSalesRepository.java
@@ -7,5 +7,42 @@
 
 package com.niit.jdp.repository;
 
+import com.niit.jdp.model.StudentData;
+import com.niit.jdp.service.StudentDatabaseService;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentSalesRepository {
+    Connection connection;
+    StudentDatabaseService service;
+
+    public StudentSalesRepository() throws SQLException, ClassNotFoundException {
+        service=new StudentDatabaseService();
+        connection= service.getConnectionToDatabase();
+    }
+    public List<StudentData> getAllStudentDatabase(){
+        List<StudentData> list=new ArrayList<>();
+
+        String selectQuery="SELECT * FROM `school`.`schooltable`;";
+        try(Statement statement=connection.createStatement()){
+            ResultSet resultSet = statement.executeQuery(selectQuery);
+            while (resultSet.next()){
+                int rollNo = resultSet.getInt("rollNo");
+                String name = resultSet.getString("name");
+                String grades = resultSet.getString("grades");
+                StudentData studentData=new StudentData(rollNo,name,grades);
+                list.add(studentData);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+return list;
+        }
 }
