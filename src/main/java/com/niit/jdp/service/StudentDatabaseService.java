c522e93432ccb95a5d9bb32bd5c2ffc76332f81b
ankit <ankit.kishorelv@gmail.com>
2022-11-28 17:11:19 +0530

    wrote a connected method and to check the data base is connected or not.

--- src/main/java/com/niit/jdp/service/StudentDatabaseService.java
+++ src/main/java/com/niit/jdp/service/StudentDatabaseService.java
@@ -7,5 +7,31 @@
 
 package com.niit.jdp.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class StudentDatabaseService {
+    Connection connection;
+    public  StudentDatabaseService(){
+        connection=null;
+    }
+    private void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/school","root","Ankit");
+
+    }
+    public Connection getConnectionToDatabase() throws SQLException, ClassNotFoundException {
+        connect();
+        return connection;
+    }
+
+    public void printConnectionStatus() {
+        if (connection != null) {
+            System.out.println("\u001B[32m Connected to Database \u001B[0m");
+        } else {
+            System.err.println("!! Database Connection Failed !!");
+        }
+    }
+
 }
