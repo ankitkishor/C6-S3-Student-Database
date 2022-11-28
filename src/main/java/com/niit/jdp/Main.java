49985d7c4a40d4e98cba0e6380189eeee3374a5c
ankit <ankit.kishorelv@gmail.com>
2022-11-28 17:12:50 +0530

    created a Main.java class so we can check the rep is connected or not

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -1,7 +1,20 @@
 package com.niit.jdp;
 
+import com.niit.jdp.service.StudentDatabaseService;
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StudentDatabaseService service=new StudentDatabaseService();
+        try {
+            service.getConnectionToDatabase();
+            service.printConnectionStatus();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
