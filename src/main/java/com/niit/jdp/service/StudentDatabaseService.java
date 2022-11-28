a27e81939ef358db30fe95efa8cf9c5c3509a716
ankit <ankit.kishorelv@gmail.com>
2022-11-28 16:25:20 +0530

    created a java file name StudentDatabaseService.java

---
+++ src/main/java/com/niit/jdp/service/StudentDatabaseService.java
@@ -0,0 +1,11 @@
+/*
+ * Author : Ankit Kishor
+ * Date: 28-11-2022
+ * Created with :Intellij IDEA Community Edition
+ */
+
+
+package com.niit.jdp.service;
+
+public class StudentDatabaseService {
+}
